0f7e7034f965d315fddc4154eea15c54101acc77
endrit <eh41767@ubt-uni.net>
2022-03-10 14:08:04 +0100

    Vue commit

---
+++ admin/pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <button @click="onClick">elo</button>
+
+    <input v-model="name">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: "nema jeff"
+      };
+    },
+    methods: {
+      onClick() {
+        alert(this.name);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/components/FeaturedProduct.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mainContent">
+    <div class="unified_widget pageBanner">
+      <h1>Books</h1>
+
+      <p>
+        <b>
+          Browse for more Products
+          <a href="#">Leaders of notable people</a>, or
+          <a href="#">Fantasy Books</a> like Harry Potter or Game of thrones
+          <a href="#">Best Fantasy books of the year!</a>
+        </b>
+      </p>
+    </div>
+    <div class="bx-root">
+      <div class="row">
+        <div class="col-sm-5 text-center">
+          <div class="bx-root-image">
+            <img src="/img/featuretProduct.jpg" class="img-fluid">
+          </div>
+        </div>
+        <div class="col-sm-7 m-auto">
+          <div class="bx-root-dtls">
+            <div class="bx-root-heading">Title Test</div>
+            <div class="bx-root-subtext">
+              Subtitle Test Lorem ipsum dolor sit amet.
+            </div>
+            <div class="bx-root-link">
+              <span>Order Now!</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
