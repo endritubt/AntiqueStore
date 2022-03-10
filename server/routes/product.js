38e97815e1fca1c34fb302d0503a51f8b8a4af19
endrit <eh41767@ubt-uni.net>
2022-03-10 13:44:18 +0100

    refined the get method, added a new startup script

--- server/routes/product.js
+++ server/routes/product.js
@@ -33,7 +33,7 @@ router.post('/products', upload.single('photo'), async (req, res) => {
 // GET - Multiple
 router.get('/products', async (req, res) => {
   try {
-    let products = await Product.find();
+    let products = await Product.find().populate('owner category').exec();
     res.json({
       success: true,
       products: products,
