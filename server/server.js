9aaba9271db9abf7721b50572374bbd368d65ae7
shqipi <shqipi_r@hotmail.com>
2022-03-11 23:04:19 +0100

    UserSingUpAPI

--- server/server.js
+++ server/server.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
 const connectionParams = {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-mongoose.connect(
-  process.env.DATABASE, 
-  connectionParams, err => {
+mongoose.connect(process.env.DATABASE, connectionParams, (err) => {
   if (err) {
     console.error(err);
   } else {
-    console.log('Connected to the Database!')
+    console.log("Connected to the Database!");
   }
-})
+});
 
 // Middlewares
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Require APIs
-const productRoutes = require('./routes/product');
-const categoryRoutes = require('./routes/category');
-const ownerRoutes = require('./routes/owner');
+const productRoutes = require("./routes/product");
+const categoryRoutes = require("./routes/category");
+const ownerRoutes = require("./routes/owner");
+const userRoutes = require("./routes/auth");
 
-app.use('/api', productRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', ownerRoutes);
+app.use("/api", productRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", ownerRoutes);
+app.use("/api", userRoutes);
 
 app.listen(3000, (err) => {
   if (err) {
@@ -44,4 +44,4 @@ app.listen(3000, (err) => {
   } else {
     console.log("listening on port 3000");
   }
-})
+});
