9c68472db9c4dc680192482954fea883ae15cb36
shqipi <shqipi_r@hotmail.com>
2022-03-12 00:19:16 +0100

    AuthAndCreatNewUser

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user");
+const verifyToken = require("../middlewares/verify-token");
 
 const jwt = require("jsonwebtoken");
 
@@ -29,6 +30,55 @@ router.post("/auth/signup", async (req, res) => {
         message: err.message,
       });
     }
+  }
+});
+
+/* Profile Route */
+router.get("/auth/user", verifyToken, async (req, res) => {
+  try {
+    let foundUser = await User.findOne({ _id: req.decoded._id });
+    if (foundUser) {
+      res.json({
+        success: true,
+        user: foundUser,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+/*Login Route*/
+router.post("/auth/login", async (req, res) => {
+  try {
+    let foundUser = await User.findOne({ email: req.body.email });
+    if (!foundUser) {
+      res.status(403).json({
+        success: false,
+        message: "Authentication failed, User not found",
+      });
+    } else {
+      if (foundUser.comparePassword(req.body.password)) {
+        let token = jwt.sign(foundUser.toJSON(), process.env.SECRET, {
+          expiresIn: 604800,
+        });
+
+        res.json({ success: true, token: token });
+      } else {
+        res.status(403).json({
+          success: false,
+          message: "Authentication failed, Wrong password!",
+        });
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
   }
 });
 
